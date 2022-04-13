98578afdd30dfdf0e3c87dc29d1f6ed49438319e
Carsten Hansen <cha@fcoo.dk>
2022-04-13 15:00:31 +0200

    Clearify comments in the script

--- example/NOOS_tides.py
+++ example/NOOS_tides.py
@@ -68,16 +68,16 @@ To get STATIONS list from model output directory
 -- See at end -- 
 """
 
-STATIONS=["A121TG","AABENRAA","AARHUS","ABERDEEN","ALS_ODDE","ALSODDE","ALTEWESERTG","ASSENS","BAGENKOP","BALLEN","BALLUM","BALLUM_SLUSE","BANDHOLM","BARSEBACK","BARSEBAECK","BERGEN","BOGENSE","BORKUM","BOULOGNE_SURMER","BOURNEMOUTH","BREMERHAVEN","BROENS_SLUSE","BRONS","BROUWERSHAVENSEGAT8TG","BSH_Arkona_Sea","BSH_Darss_Sill","BSH_DeutcheBucht","BSH_Elbe","BSH_Ems","BSH_Fehmarn_Belt","BSH_Fino1","BSH_Fino2","BSH_Fino3","BSH_Kiel","BSH_NsbII","BSH_NsbIII","BSH_Oder","BUESUMTG","BUOY_HVEN_KNOELEN","CALAIS","CHERBOURG","CROMER","CUXHAVEN","D151TG","DAGEBUELLTG","DAUGAVGRIVA","DEGERBY","DELFZIJL","DEN_HELDER","DIEPPE","DOVER","DRAGOER","DROGDEN","DUNKERQUE","EIDERSPTG","ESBJERG","EUROPLATFORMTG","F161","F3PLATFORMTG","FAABORG","FALKENBERG","FCOO_DROGDEN","FCOO_GREATBELTBRIDGE_EAST","FCOO_VENGEANCE","FELIXSTOWE","FERRING_KYST","FLINTR_NORD","FLINTR_SYD","FORSMARK","FREDERICIA","FREDERIKSSUND_S","FREDERIKSVAERK","FR_HAVN","FUROOGRUND","FYNSHAV","GABET","GEDSER","GOTEBORG","GRAADYB_BARRE","GRENAA","GULDBORGSUNDTUNNEL_FALSTER","GULDBORGSUNDTUNNEL_LOLLAND","HADERSLEV","HALMSTAD","HALS","HALS_BARRE","HANSTHOLM","HARINGVLIET10TG","HARWICH","HAVNEBY","HELGEROA","HELGOLAND","HELSINGBORG","HELSINKI","HESNAES","HIRTSHALS","HOEKVANHOLLAND","HOERNUM","HOLBAEK","HORNBAEK","HOV","HUIBERTGATTG","HUNDESTED","HUNDIGE","HUSUM","HVIDESANDE_HAVN","HVIDESANDE_KYST","IJMONDSTROOMPLAL","IJMUIDEN","IMMINGHAM","IOC_DLPR","IOC_SDPR","J61","J6PLATFORM","JAEGERSPRIS_KIGNAES","JUELSMINDE","K13ALPHA","K13ATG","K141TG","KALIX","KALUNDBORG","KALVEHAVE","KARREBAEKSMINDE","KASKINEN","KASTRUP","KEMI","KERTEMINDE","KIEL","KLAGSHAMN","KLAIPEDA","KLINTHOLM","KOEBENHAVN","KOLDING","KOLKA","KORSOER","KOSEROW","KUNGSHOLMSFORT","KUNGSVIK","KYNDBYVAEVAERKET","L91TG","LANDSORT","LE_CONQUET","LE_HAVRE","LEITH","LICHTEILANDGOEREE","LIST","LOWESTOFT","LTL_BELT_CURR","MALMO","MANDOE","MARSTRAND","MARVIKEN","NAKSKOV","NDRROESE","NEWHAVEN","NEWLYN","NIEUWPOORT","NORDERNEYTG","NORTHSHIELDS","NYKOEBINGSJAELLAND","ODDENHAVN","ODENSE","ONSALA","OOSTENDE","OOSTERSCHELDE11TG","OSCARSBORG","OSKARSHAMN","OSLO","PIETARSAARI","PLYMOUTH","PORTSMOUTH","PRAESTOE","Q11TG","RATAN","RIBE_KAMMERSLUSE","RIBE_KYST","RINGHALS","ROEDBYHAVN","ROEDVIG","ROEMOE","ROENNE","ROERVIG","ROOMPOTBUITEN","ROSCOFF","ROSKILDE","ROUTE_ALPHA_WEST","SAINT_MALO","SASSNITZ","SHEERNESS","SILLAMAE","SIMRISHAMN","SKAGEN","SKAGSUDDE","SKANOER","SLETTEN","SLETTEN_HAVN","SLIPSHAVN","SMOGEN","SOENDERBORG","SPIKARNA","STAVANGER","STEGE","STHELIER","STMARYSTG","STOCKHOLM","STPETERSBURG","SVENDBORG","TALLINN","TEJN","TERSCHELLING","THORSMINDE_HAVN","THORSMINDE_KYST","THYBOROEN_HAVN","THYBOROEN_KYST","TRAVEMUNDE","TREGDE","UDBYHOEJ","VARBERG","VEDBAEK","VESTEROE","VIDAA","VIDAASLUSEN","VIKEN","VIKER","VINGA","VISBY","VLAKTEVDRAANTG","VLISSINGEN","VORDINGBORG","WANGEROOGETG","WARNEMUNDE","WEYMOUTH","WHITBY","WICK","WIERUMERGRONDENTG","WITTDUENTG","YSTAD","ZINGST","SMHI_AA13","SMHI_AA15","SMHI_AA17","SMHI_ANHOLT_E","SMHI_BCSIII-10","SMHI_BROFJORDENWR","SMHI_BY10","SMHI_BY11","SMHI_BY1","SMHI_BY13","SMHI_BY15","SMHI_BY19","SMHI_BY20","SMHI_BY21","SMHI_BY2","SMHI_BY27","SMHI_BY29","SMHI_BY31","SMHI_BY32","SMHI_BY36","SMHI_BY38","SMHI_BY4","SMHI_BY5","SMHI_BY7","SMHI_BY9","SMHI_C3","SMHI_F3","SMHI_F9","SMHI_FLADEN","SMHI_HANOBUKTEN","SMHI_HUVUDSKAROST","SMHI_LAESO_RENNE","SMHI_LASO_E","SMHI_MS4","SMHI_P2","SMHI_STOLPETROSKEL","SMHI_TANGESUND","SMHI_VADEROARNA","SMHI_WLANDSKRONA","DMU_Aalborg_bugt","DMU_Gniben","DMU_LangeL_Belt","DMU_Romsoe","FRV_Drogden","FRV_Venegance","FRV_W26","IOW_TF0245","IOW_TF0271b","IOW_TF0284","IOW_TF0286","NOVANA_Hjelm_bugt","NOVANA_Lillebelt","PILOT_AABENRAA","PILOT_AARHUS_STUDSTRUP","PILOT_AROE_SE","PILOT_AVEDOEREVAERKET","PILOT_BANDHOLM","PILOT_BORNHOLM_E","PILOT_BORNHOLM_N","PILOT_BORNHOLM_W","PILOT_BREDEGRUND","PILOT_CHARLIE_4","PILOT_DROGDEN","PILOT_ESBJERG","PILOT_FREDERIKSHAVN","PILOT_FYNSHAV","PILOT_FYNS_HOVED","PILOT_GABET","PILOT_GEDSER","PILOT_GRENAA","PILOT_HALS_1","PILOT_HALS_2","PILOT_HALS_3","PILOT_HANSTHOLM","PILOT_HESTEHOVED","PILOT_HIRTSHALS","PILOT_HVIDE_SANDE","PILOT_ISEFJORD","PILOT_KADETRENDEN_1","PILOT_KALUNDBORG_RED","PILOT_KOEBENHAVN_RED","PILOT_KOEGE","PILOT_KOLDING","PILOT_KORSOER","PILOT_KORSOER_RED","PILOT_LANGELAND_H","PILOT_LANGELAND_T","PILOT_LEHNSKOV","PILOT_LILLEGRUND","PILOT_LIMFJORDEN_E","PILOT_LOEGSTOER_GRUNDE","PILOT_MARIAGER","PILOT_MARSTAL","PILOT_NAESTVED","PILOT_NAKSKOV","PILOT_NEXOE","PILOT_NORDBORG","PILOT_NYBORG","PILOT_ODDESUNDBROEN_NE","PILOT_ODDESUNDBROEN_SW","PILOT_POELS_REV","PILOT_RANDERS","PILOT_ROEDBY_HAVN","PILOT_ROEMOE","PILOT_ROENNE","PILOT_ROESNAES","PILOT_SKAGEN_1","PILOT_SKAGEN_2","PILOT_SKAGEN_3","PILOT_SKAGEN_4","PILOT_SKAGEN_HAVN","PILOT_SKELLELEV","PILOT_SKRAMS_FLAK","PILOT_SOENDERBORG_N","PILOT_SOENDERBORG_S","PILOT_SPROGOE_NE","PILOT_SUNDET_N","PILOT_THUROE_REV","PILOT_THYBOROEN","PILOT_TRAGTEN_NE","PILOT_TRELDE_NAES","SYKE_ARGOS_BALTPROP1","SYKE_ARGOS_BOTHSEA1","SYKE_LL12","SYKE_LL15","SYKE_LL17","SYKE_LL3A","SYKE_LL7",]
-
-STATIONS=["A121TG","AABENRAA","AARHUS",]
+STATIONS=["A121TG","AABENRAA","AARHUS",] # etc.
 
 delta_time *= 60. # 10 min
 
-# num_years = len(years_analyze) # Number of full years in analysis
-num_years = 0 # If years_predict > 0: Use existing analysis results from file
-              # to make prediction
-              # If years_predict == []: Make analyses shorter that 1 year
+num_years = 0 # If len(years_predict)>0: Use existing analysis results from
+# file to make prediction.
+# If years_predict==[]: Make analyses shorter that 1 year
+# If there are no analysis results, we will set 
+# num_years = len(years_analyze) in the loop below to do the analysis
+# and then do the prediction with num_years = 0
 
 # In the logics of the main routine we presume that:
 if num_years > 0:
@@ -96,40 +96,41 @@ if not setup_anal in r_ph.keys():
 
 
 """
+About the OSU constituents used in the NS1C model boundary conditions for
+elevation and transports/currents:
 
-The TPXO models include complex amplitudes of MSL-relative sea-surface
+"The TPXO models include complex amplitudes of MSL-relative sea-surface
 elevations and transports/currents for eight primary (M2, S2, N2, K2, K1,
 O1, P1, Q1), two long period (Mf,Mm) and 3 non-linear (M4, MS4, MN4)
 harmonic constituents (plus 2N2 and S1 for TPXO9 only).
 
 TPXO9 versions starting from v2 and v5a include minor constituents:
-2Q1, J1, L2, M3, MU2, NU2, OO1.
+2Q1, J1, L2, M3, MU2, NU2, OO1."
 
-??? are they actually there?: Mm, S1, J1, OO1 ???
 """
 
-# Restrict to the OSU constituents + possibly inferred constituents
-TPXOall = ["M2", "O1", "S2","Mf", "Mm", "Q1", "K2",  "K1", "N2", "P1",
+# Restrict to the possible OSU constituents + possible inferred constituents
+HARMall = ["M2", "O1", "S2", "Mf", "Mm", "Q1", "K2",  "K1", "N2", "P1",
            "S1", "2Q1",  "J1",  "L2", "mu2", "nu2", "OO1",
            "2N2", "MN4","M4", "M6", "M8", "MS4", "2MS6" ]
 
 # For short analysis ( <~ 28 days )
-TPXOshort = ["M2", "K1", ]
+HARMshort = ["M2", "K1", ]
 
 # In longer analysis
-TPXOlong = [ "O1", "S2", "N2", "Q1", "K2", "Mf", "Mm",   ]
+HARMlong = [ "O1", "S2", "N2", "Q1", "K2", "Mf", "Mm",   ]
 
 # In very long analysis
-TPXOminor = [ "P1", "S1", "2Q1",  "J1",  "L2", "mu2", "nu2", "OO1" ]
+HARMminor = [ "P1", "S1", "2Q1",  "J1",  "L2", "mu2", "nu2", "OO1" ]
 
-TPXOnonlin = [ "M4","2N2", "MS4", "MN4", "M6", "M8", "2MS6" ]
+HARMnonlin = [ "M4","2N2", "MS4", "MN4", "M6", "M8", "2MS6" ]
 # Ignore M3; add M6, M8, "2MS6" as inferred
 
-TPXOanalyse = TPXOshort
-TPXOlong += TPXOminor + TPXOnonlin
-assert len(set(TPXOall)) == len(set(TPXOanalyse)) + len(set(TPXOlong))
-assert set(TPXOall) == set(TPXOanalyse + TPXOlong)
-assert len(TPXOall) == len(TPXOanalyse) + len(TPXOlong)
+HARManalyse = HARMshort
+HARMlong += HARMminor + HARMnonlin
+assert len(set(HARMall)) == len(set(HARManalyse)) + len(set(HARMlong))
+assert set(HARMall) == set(HARManalyse + HARMlong)
+assert len(HARMall) == len(HARManalyse) + len(HARMlong)
 
 # Declare a global parameters
 regs = {'seriesfile': '',      # Name of timeseries netcdf file
@@ -286,23 +287,23 @@ def read_timeseries( year, STATION, delta_time, num_years, verbose=1):
     
     return time[:tindx0], elev[:tindx0], tindx0, time_units
 
-def print_speed_list(x,TPXOall,TPXOminor=[]):
+def print_speed_list(x,HARMall,HARMminor=[]):
     speed_list=[]
-    for harm in TPXOall:
+    for harm in HARMall:
         speed_list+=[x.tidal_dict[harm]['ospeed']]
     speed_list = np.asarray(speed_list)
     
     speed_argsort = np.argsort(speed_list)
-    TPXO_sorted = [ TPXOall[ii] for ii in speed_argsort ]
+    HARM_sorted = [ HARMall[ii] for ii in speed_argsort ]
     speed_sorted = np.rad2deg(speed_list[speed_argsort])
     speed_dists =  speed_sorted[1:]-speed_sorted[:-1]
-    harm=TPXO_sorted[0]
+    harm=HARM_sorted[0]
     print ''
     print 'Harmonics phase, delta-phase [*:minor]'
     print '    %4s %5.2f' % (harm,speed_sorted[0])
-    for ii in range(1,len(TPXO_sorted)):
-        harm=TPXO_sorted[ii]
-        if harm in TPXOminor:
+    for ii in range(1,len(HARM_sorted)):
+        harm=HARM_sorted[ii]
+        if harm in HARMminor:
             print '   *',
         else:
             print '    ',
@@ -365,6 +366,9 @@ asc.package['dates'] = dict()
 asc.packagefil = ''
 
 def package_from_file(datesid,datestamp=None):
+
+    # Read the saved astronomics factors of all the dates
+    
     asc.packagefil = ''
     try:
         # Re-use earlier data (e.g. from the first STATION)
@@ -388,12 +392,17 @@ def package_from_file(datesid,datestamp=None):
         asc.package['dates'] = dict()
   
 def package_to_file():
+
+    # Save the astronomics factors of all the dates to file
+    
     if asc.packagefil is not 'New': return
     print 'Save new/updated astronomics package to file', asc.package['filename']
     with open(asc.package['filename'],'w') as packagefil:
         pickle.dump(asc.package,packagefil)
   
 def tappy_fill(u, dates, datesid, start=0, end=0, verbose = 1):
+
+    # Fill the tappy instance with the atronomics factors for all dates
     
     if end == 0: end = len(dates)
     u.dates = dates[start:end]
@@ -482,8 +491,7 @@ while stii < len(STATIONS):
         num_years = len(years_analyze)
         print '%s: Analyze for constituents based on the %d years long period.'%\
               (STATION, num_years),
-        # if STATION in ncfile:
-        #     raise RuntimeError, 'Fail: Cannot run the analysis twice'
+
         if years_predict0: 
             stii-=1
             print 'The prediction is run thereafter for the station'
@@ -501,7 +509,7 @@ while stii < len(STATIONS):
     if len(years_predict) > 0:
 
         harm = r_ph[setup_anal][STATION]['harm_sorted']        
-        assert set(harm) == set(TPXOall)
+        assert set(harm) == set(HARMall)
 
         rr = dict()
         ph = dict()
@@ -520,7 +528,7 @@ while stii < len(STATIONS):
         print 'prediction = sum of signals ...'
         sys.stdout.flush()
         
-        pred_all = u.sum_signals(TPXOall, calcdates, u.tidal_dict)
+        pred_all = u.sum_signals(HARMall, calcdates, u.tidal_dict)
 
         # Output to netCDF
         pred_to_netCDF(STATION, ncfile, x, harm, rr, ph, pred_times, pred_all)
@@ -540,7 +548,6 @@ while stii < len(STATIONS):
     end = int(np.rint( num_years * 365.25 * 86400. / delta_time ))
     
     package_id = '%d-%d' %(years_analyze[0],years_analyze[-1])
-    TPXOanalyse = TPXOall # Include them all
   
     if verb > 0: print dates[start],
             
@@ -553,9 +560,9 @@ while stii < len(STATIONS):
                              verbose=verb)
     
     if verb > 1:
-        print_speed_list(x,TPXOall,TPXOminor)
+        print_speed_list(x,HARMall,HARMminor=HARMminor)
     
-    x.key_list = TPXOanalyse
+    x.key_list = HARMall # Include them all
         
     x.speed_dict = dict()
     for harm in x.key_list:
@@ -634,52 +641,6 @@ done
 stations+=']'
 insts_stations=$stations
 echo STATIONS=$insts_stations
-"""
-
-"""
-The TPXO models include complex amplitudes of MSL-relative sea-surface
-elevations and transports/currents for eight primary (M2, S2, N2, K2, K1,
-O1, P1, Q1), two long period (Mf,Mm) and 3 non-linear (M4, MS4, MN4)
-harmonic constituents (plus 2N2 and S1 for TPXO9 only).
-
-TPXO9 versions starting from v2 and v5a include minor constituents:
-2Q1, J1, L2, M3, MU2, NU2, OO1.
-
-??? are they actually there?: Mm, S1, J1, OO1 ???
-
-Example:
-STATION='Esbjerg'
-In [142]: r_ph['NS1C-v014Eey_2017-2021'][STATION]
-Out[142]: 
-{'2MS6': [0.0120541135154026, 160.08283761904272],
- '2N2': [0.012012773820247345, 333.269936674574],
- '2Q1': [0.00935842767686989, 30.932824644807226],
- 'J1': [0.006021099639815467, 341.6079594520876],
- 'K1': [0.06235063763639531, 247.3596249994372],
- 'K2': [0.043412586825639796, 105.82165319552773],
- 'L2': [0.05208757619934658, 50.258894990724],
- 'M2': [0.6068451437337439, 34.17371342742524],
- 'M4': [0.04347045162670171, 275.32913898048565],
- 'M6': [0.01561960589739096, 98.15441427764404],
- 'M8': [0.007909296125413847, 24.008473220415738],
- 'MN4': [0.017940684733465242, 243.93372501802438],
- 'MS4': [0.021118950852473235, 356.01240020511955],
- 'Mf': [0.008895515179259964, 92.56274639153037],
- 'Mm': [0.010223486111328499, 196.1828284994217],
- 'N2': [0.09493328610145506, 5.217712232458894],
- 'O1': [0.07132651560599501, 87.91793760194619],
- 'OO1': [0.004533469542120409, 310.8794670672363],
- 'P1': [0.017251795721901077, 114.96055274164033],
- 'Q1': [0.051673095579873214, 52.28976746292568],
- 'S1': [0.004148033726305362, 237.50197853334407],
- 'S2': [0.14259295383554674, 95.98382020777899],
- 'mu2': [0.057919184684684225, 142.65929491787293],
- 'nu2': [0.03619309534140167, 2.56407978624361],
-'harm_sorted': ['M2',  'S2',  'N2',  'O1',  'K1',  'mu2',  'L2',  'Q1',
-'M4',  'K2',  'nu2',  'MS4',  'MN4',  'P1',  'M6',  '2MS6',  '2N2',
-'Mm',  '2Q1',  'Mf',  'M8',  'J1',  'OO1',  'S1']}
-
-# Only S1, J1, OO1 found negligible
 """
 
 
